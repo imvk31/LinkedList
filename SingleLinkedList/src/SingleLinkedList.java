5743f77651da65344a587df70acf0f7e5d4ec3d5
Varam <prasadkurra31@gmail.com>
2021-09-12 10:13:37 -0400

    Created a simple LinkedList.

---
+++ SingleLinkedList/src/SingleLinkedList.java
@@ -0,0 +1,48 @@
+class SingleLinkedList
+{
+    class Node
+    {
+        int data;
+        Node next= null;
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head = null;
+    Node tail = null;
+
+    public void add(int data)
+    {
+        Node newNode = new Node(data);
+        if(head == null) {
+            newNode.next = null;
+            head= newNode;
+        }
+        else{
+            tail.next = newNode;
+        }
+        tail = newNode;
+    }
+
+    public void print()
+    {
+        Node curr = head;
+        while(curr!=null)
+        {
+            System.out.print(curr.data+"->");
+            curr = curr.next;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String args [])
+    {
+        SingleLinkedList myList = new SingleLinkedList();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.print();
+    }
+}
